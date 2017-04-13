23c59e17d03c049b22e3178912dc8faf515465d9
leknaat <wishfire@gmail.com>
2017-04-13 13:03:17 +0100

    Initial commit

---
+++ src/redux/actions/todos.js
@@ -0,0 +1,15 @@
+export const addTodo = (todo) => {
+  return { type: 'todos/ADD_TODO', payload: todo }
+}
+
+export const updateTodoText = (todoText) => {
+  return { type: 'todos/UPDATE_TODO_TEXT', payload: todoText }
+}
+
+export const completeTodo = (todoId) => {
+  return { type: 'todos/COMPLETE_TODO', payload: todoId }
+}
+
+export const deleteTodo = (todoId) => {
+  return { type: 'todos/DELETE_TODO', payload: todoId }
+}
